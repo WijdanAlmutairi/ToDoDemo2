7d11d96b677d03227c2c3d443abf58873c07c71f
Wijdan Almutairi <wijdan.s.almutairi@gmail.com>
2024-10-01 17:08:22 +0300

    Initial Commit

---
+++ ToDoDemo2/ToDoDemo2App.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoDemo2App.swift
+//  ToDoDemo2
+//
+//  Created by Wijdan Almutairi on 01/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoDemo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
